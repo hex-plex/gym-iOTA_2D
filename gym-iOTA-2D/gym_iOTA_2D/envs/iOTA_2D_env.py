5fbdb62f5c5f8b4296d179dfa630420a84708a98
Dev Raj R <dev.raj.r1729@gmail.com>
2021-07-21 01:23:20 +0530

    world setup in reset, replaced damping with friction

--- gym-iOTA-2D/gym_iOTA_2D/envs/iOTA_2D_env.py
+++ gym-iOTA-2D/gym_iOTA_2D/envs/iOTA_2D_env.py
@@ -20,9 +20,14 @@ class Iota2DEnv(gym.Env):
         self.target_pos = (0,5)
         self.box_side = 1
         self.robot_radius=0.3
-        self.max_velocity = 1.
-        self.max_force = 1.
+        self.max_velocity = 2.
+        self.max_force = 2.
         self.epsilon = 0.01
+        self.step_fps_ratio = 10
+        
+        self.robot_friction_coefficient = 0.3
+        self.box_friction_coefficient = 0.3
+        self.gravity = 10
         high = np.array([(np.array(self.arena,dtype=np.float64)) for _ in range(self.n)])
 
         self.action_space = spaces.Box(-high,high,
@@ -37,37 +42,9 @@ class Iota2DEnv(gym.Env):
             shape=(self.n,2)    #sanity check
         )
 
-        # world setup
-        self.screen=None
-        self.world = world(gravity=(0,0))
-        # TODO : positions
-        self.box = self.world.CreateDynamicBody(position=(0,0),linearDamping=5,angularDamping=5)
-        self.box_fixture = self.box.CreatePolygonFixture(
-            box=(self.box_side,self.box_side),
-            density=1,
-            friction=0
-            )
-        xchoices =[]
-        ychoices = []
-        for i in range (-80,85,5):
-            if np.abs(i)>20:
-                xchoices.append(i/10.)
-                ychoices.append(i/10.)
-        np.random.shuffle(xchoices)
-        np.random.shuffle(ychoices)
-        print(list(zip(xchoices[:self.n],ychoices[:self.n])))
-        positions = list(zip(xchoices[:self.n],ychoices[:self.n]))
-        self.robots = [
-            self.world.CreateDynamicBody(position=position) for position in positions
-        ]
-
-        self.robots_fixtures = [
-            robot.CreateCircleFixture(radius=self.robot_radius,density=1,friction=0) 
-            for robot in self.robots
-        ]
         self.screen_width,self.screen_height = 2*np.array(self.arena)*self.pixels_per_metre
         self.fps = 60
-        self.time_step = 1./self.fps
+        self.time_step = 1./(self.fps*self.step_fps_ratio)
 
         colors = {
             'box': (255,255,255,255),
@@ -93,11 +70,12 @@ class Iota2DEnv(gym.Env):
 
     def step(self,action):
         err_msg = "%r (%s) invalid" % (action,type(action))
-        assert self.action_space.contains(action), err_msg
-        # TODO implement step 
+        assert self.action_space.contains(action), err_msg 
         finished = False
+        step=0
         while not finished:
             finished =True
+
             for robot,destination in zip(self.robots,action):
                 delta = vec2(destination) - robot.position
                 if delta.length <= self.epsilon:
@@ -107,18 +85,59 @@ class Iota2DEnv(gym.Env):
                 direction = delta/delta.length
                 vel_mag = robot.linearVelocity.length * direction.dot(robot.linearVelocity)
                 force_mag = self.max_force*(1 - vel_mag/self.max_velocity)
-                robot.ApplyForce(force = force_mag*direction,point=robot.position,wake=True)
+                force = force_mag*direction
+                if robot.linearVelocity.length!=0:
+                    force-= self.robot_friction * robot.linearVelocity/robot.linearVelocity.length
+                robot.ApplyForce(force = force,point=robot.position,wake=True)
+                
+            if self.box.linearVelocity.length != 0:
+                self.box.ApplyForceToCenter(
+                    force=-self.box_friction*self.box.linearVelocity/self.box.linearVelocity.length,
+                    wake=True)        
 
             self.world.Step(self.time_step,10,10)
-            self.render() 
-        self.world.Step(self.time_step,10,10) 
+            if step%self.step_fps_ratio == 0:
+                self.render() 
+            step = (step+1)%self.step_fps_ratio
+        self.world.Step(self.time_step,10,10)
+        self.world.ClearForces()
         observation = np.array([np.array(robot.position) for robot in self.robots])
-        dx,dy = self.box.position - self.target_pos
-        reward = -(dx**2 + dy**2)
-        done = reward == 0
-        return observation,reward, done, { }
+        target_delta = self.box.position - self.target_pos
+        done = target_delta.length<= self.epsilon
+        return observation,-target_delta.lengthSquared, done, { }
 
     def reset(self):
+        self.screen=None
+        self.world = world(gravity=(0,0))
+        # TODO : positions
+        self.box = self.world.CreateDynamicBody(position=(0,0),angularDamping=5)
+        self.box_fixture = self.box.CreatePolygonFixture(
+            box=(self.box_side,self.box_side),
+            density=1,
+            friction=0
+            )
+        self.box_friction = self.box.mass*self.gravity*self.box_friction_coefficient
+        xchoices =[]
+        ychoices = []
+        for i in range (-80,85,5):
+            if np.abs(i)>20:
+                xchoices.append(i/10.)
+                ychoices.append(i/10.)
+        np.random.shuffle(xchoices)
+        np.random.shuffle(ychoices)
+        print(list(zip(xchoices[:self.n],ychoices[:self.n])))
+        positions = list(zip(xchoices[:self.n],ychoices[:self.n]))
+        self.robots = [
+            self.world.CreateDynamicBody(position=position) for position in positions
+        ]
+
+        self.robots_fixtures = [
+            robot.CreateCircleFixture(radius=self.robot_radius,density=1,friction=0) 
+            for robot in self.robots
+        ]
+
+        self.robot_friction = self.robots[0].mass*self.gravity*self.robot_friction_coefficient
+
         print(np.array(self.robots[0].position))
         print('reset-works')
         return np.array([np.array(robot.position) for robot in self.robots])
